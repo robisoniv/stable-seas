82c4eec6a5d2a2236f6c7e72b17e25729d032292
John Robison Hoopes <iv@Johns-MacBook-Pro-2.local>
2018-02-05 11:57:27 -0700

    Updated path to executive summaries

--- js/main.js
+++ js/main.js
@@ -159,7 +159,7 @@ if (width > 1199 || window.navigator.userAgent.indexOf('MSIE') != -1) {
 
 // Ad hoc elements - ### Add these to templates !
 
-$('#resources-menu').prepend('<li><a href="../../stable-seas-executive-brief.pdf" target="_blank">Executive Summary</a></li>');
+$('#resources-menu').prepend('<li><a href="../../assets/summaries/stable-seas-' + issueAreaData[issueArea].metadata.path + '-executive-summary.pdf" target="_blank">Executive Summary</a></li>');
 
 
 // 3 Event listeners
