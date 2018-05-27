7401576aaa41e320618e23636d4b1ccdd0887d8f
ksoeth <36307976+ksoeth@users.noreply.github.com>
2018-05-27 15:30:08 -0600

    DRC
    DRC content

--- data/regions-data.js
+++ data/regions-data.js
@@ -2804,15 +2804,15 @@ var regionsData = {
           },
           {
             "tag": "p",
-            "html": "Across the nine issues covered by the Stable Seas Maritime Security Index, DRC ranks highest in International Cooperation. DRC's score of 56 in this area is high relative to its other issue scores,          due in part to participation in the Yaounde Code of Conduct and the African Union's Lome Charter.. DRC can further improve upon this area of relative strength by resolving maritime border disagreements with Angola and ratifying and signing more global international agreements on maritime issues."
+            "html": "Across the nine issues covered by the Stable Seas Maritime Security Index, DRC ranks highest in International Cooperation. DRC's score of 56 in this area is high relative to its other issue scores,          due in part to participation in the Yaoundé Code of Conduct and the African Union's Lomé Charter. DRC can further improve upon this area of relative strength by resolving maritime border disagreements with Angola and ratifying and signing more global international agreements on maritime issues."
           },
           {
             "tag": "bigtext",
-            "html": "DRC is also above the regional average in Illicit Trades."
+            "html": "DRC is also above the regional average in Illicit Trade."
           },
           {
             "tag": "p",
-            "html": "The greatest remaining challenge for DRC is Blue Economy. DRC scores 20 in this area, below the regional average of 44. To further             improve upon this score, DRC can work with Angola to settle maritime boundary disputes, which will improve the country's ability to profit from its fisheries.. Further progress can be made by continuing to invest in port infrastructure at Matadi and Boma.. Through these efforts, DRC can raise its weakest score and work toward comprehensive and lasting maritime security."
+            "html": "The greatest remaining challenge for DRC is Blue Economy. DRC scores 18 in this area, below the regional average of 40. To             improve this score, DRC can work with Angola to settle maritime boundary disputes, which will improve the country's ability to profit from its fisheries and other types of devlopment. Further progress can be made by continuing to invest in port infrastructure at Matadi and Boma. Through these efforts, DRC can raise its weakest score and work toward comprehensive and lasting maritime security."
           }
         ]
       }
